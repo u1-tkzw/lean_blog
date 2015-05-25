31b58fcb9ced0a19ec89296357eba72295e58f0b
takazawa <takazawa@h2system.jp>
2015-05-25 15:58:26 +0900

    infobar 用に既存ソースを修正

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -7,22 +7,6 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
 
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
